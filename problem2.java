219ad416c553f7c438a78d7d38804aa58775f938
JeongHun, Kim <heavyuser73@gmail.com>
2019-12-26 11:00:57 +0900

    Update problem2.java
    변수명 수정

--- problem2.java
+++ problem2.java
@@ -10,28 +10,28 @@ class Solution {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode resultNode=null;
-        ListNode lastNode=null;
+        ListNode tempNode=null;
         int temp;
-        int addNum = 0;
+        int upNum = 0;
         int currentNum = 0;
         while(l1 !=null || l2 !=null) {
-            temp = ((l1!=null)?l1.val:0) + ((l2!=null)?l2.val:0) + addNum;
-            addNum = (int)(temp/10);
+            temp = ((l1!=null)?l1.val:0) + ((l2!=null)?l2.val:0) + upNum;
+            upNum = temp/10;
             currentNum = temp % 10;
             if(resultNode == null) {
                 resultNode = new ListNode(currentNum);
-                lastNode = resultNode;
+                tempNode = resultNode;
             }
             else {
-                lastNode.next = new ListNode(currentNum);
-                lastNode = lastNode.next;
+                tempNode.next = new ListNode(currentNum);
+                tempNode = tempNode.next;
             }
             
             l1 = (l1 != null)?l1.next:null;
             l2 = (l2 != null)?l2.next:null;
         }
-        if(addNum > 0) {
-            lastNode.next = new ListNode(addNum);
+        if(upNum > 0) {
+            tempNode.next = new ListNode(upNum);
         }
         
         return resultNode;
