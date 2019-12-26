15c50170862f7219671daf8567d1cefa3f177ea2
JeongHun, Kim <heavyuser73@gmail.com>
2019-12-26 18:17:29 +0900

    Update problem3.java

--- problem3.java
+++ problem3.java
@@ -3,15 +3,13 @@ class Solution {
         int max = 0;
         char [] strArray = s.toCharArray();
         HashSet<Character> arraySet = new HashSet<>();
-        int last=0;
+        char currentChar;
         for(int i=0;i<strArray.length;i++) {
             for(int j=i;j<strArray.length;j++) {
-                char currentChar = strArray[j];
+                currentChar = strArray[j];
                 if(arraySet.contains(currentChar) == false) {
                     arraySet.add(currentChar);
-                    if(max < arraySet.size()) {
-                        max = arraySet.size();
-                    }
+                    max = Math.max(max, arraySet.size());
                 }
                 else {
                     arraySet.clear();
