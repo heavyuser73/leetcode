bd7f46419f3d308c5b318501118ef7e4df8a4e95
JeongHun, Kim <heavyuser73@gmail.com>
2019-12-27 20:32:26 +0900

    Update problem4.java
    변수명 수정

--- problem4.java
+++ problem4.java
@@ -1,12 +1,10 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int [] result = new int [nums1.length + nums2.length];
+        int [] mergeArray = new int [nums1.length + nums2.length];
         int index1 = 0;
         int index2 = 0;
  
         for(int i=0;i< nums1.length + nums2.length;i++) {
-            
-
             if(nums1.length > index1 && nums2.length > index2) {
                 int min=0;
                 if(nums1[index1] < nums2[index2]) {
@@ -15,19 +13,19 @@ class Solution {
                 else {
                     min = nums2[index2++];
                 }
-                result[i] = min;
+                mergeArray[i] = min;
             }
             else if(nums1.length > index1) {
-                result[i] = nums1[index1++];
+                mergeArray[i] = nums1[index1++];
             }
             else {
-                result[i] = nums2[index2++];
+                mergeArray[i] = nums2[index2++];
             }
         }
-        if(result.length % 2 == 1) 
-            return  result[(result.length -1) /2];
+        if(mergeArray.length % 2 == 1) 
+            return  mergeArray[(mergeArray.length -1) /2];
         else {
-            return (result[result.length/2 -1] + result[result.length/2]) / 2.0;
+            return (mergeArray[mergeArray.length/2 -1] + mergeArray[mergeArray.length/2]) / 2.0;
         }
     }
 }
